43ce95414db94fedad6a83e0ff8c13deeacb73b6
Sbtha <shisastore@protonmail.com>
2021-07-29 14:42:22 +0200

    Added comments and killed typos in readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#This is a xlxs raport generator I made for my work. Main task of this program is to read xlsx file with yearly sales from enitire store. Then for each brand|producer program generates a new file which contains articles that we will return since they had no sales. There is a simple formula written that calculates that.
+#This is an xlxs raport generator I made for my work. Main task of this program is to read xlsx file with yearly sales from entire store. Then for each brand|producer program generates a new file which contains articles that we will return since they had no or litte sales. There is a simple formula written that calculates that.
 
-Right now if producer label have slashes('/') program is crashing. Dunno how to fix that since I'm using f-string to generate files names. Maybe format function ? 
+Right now if producer label have slashes('/') program is crashing. Dunno how to fix that since I'm using f-string to generate files names. Maybe format function ?
--- raport_generator.py
+++ raport_generator.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-
+#For clean use create a 'Raport' folder in same dir that script is located and copy original file there.
 os.chdir('Raport')
 FILE_PATH = 'Raport_All.xlsx'
  
@@ -13,6 +13,7 @@ os.mkdir('Wygenerowane')
 marki_lista = set([marka for marka in marki])
 for marka in marki_lista:
 
+    # Theese are the columns I need to generate to calculate which articles we would like to return to producer. In previous years I generated those files manually in excel that took my arround a month of work since I was doing this as en extra task.
     df['Średnia sprzedaż w Miesiącu'] = df['Ilość sprzedana'] / 12
     df['Średni zapas Na 6M'] = df['Średnia sprzedaż w Miesiącu'] * 6
     df['Stan-Zapas'] = df['Stan teoretyczny'] - \
@@ -21,17 +22,22 @@ for marka in marki_lista:
         (df['Średnia sprzedaż w Miesiącu'] * 3)
     df['Ilość do Zwrotu Zaokrąglone'] = df['Ilość do Zwrotu'].round()
 
+    
     df.sort_index(inplace=True)
 
+    # This is the main filter I use on generated columns.
     filt = (df['Stan-Zapas'] >= 1) & (df['Marka']
                                             == marka)  # this will create many filters
 
+    # This is a 2nd spreadsheet generated for lazy managers that prepares returns ;)
     raport2 = df.loc[filt, ['EAN Prio', 'Ilość do Zwrotu Zaokrąglone']]
 
+    # This part saves file with specified sheets.
     writer = pd.ExcelWriter(f"Wygenerowane/Raport_{marka}.xlsx")
     df[filt].to_excel(writer, sheet_name="Raport")
     raport2.to_excel(writer, sheet_name="Raport EAN i Ilość")
 
+    # Some information about status of program.
     print(f"Raport_{marka} - WYKONANY!")
 
     writer.save()
